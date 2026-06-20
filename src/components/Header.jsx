ab112064597ccdbcc16f0870d884dfe0a9d7f210
jobanputrayash22-cpu <jobanputrayash22@gmail.com>
2026-06-20 13:11:58 +0530

    Fix logo filename case

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import logoImg from "../assets/easyDrive-logo.png";
+import logoImg from "../assets/easydrive-logo.png";
 
 export default function Header() {
   const [user, setUser] = useState(null);
